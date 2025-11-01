3832e180a921bb9f5d583886d9f0157d065ccdf4
Jaroslaw-super1 <Jarafan3@gmail.com>
2025-11-01 15:06:12 +0500

    make

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
-int ** make(int rows, int cols) {
-
+int ** make(int r, int c) {
+    int ** mtx = new int * [r];
+    for (size_t i = 0; i < r; i++) {
+        try {
+            mtx[i] = new int[c];
+        } catch (const std::bad_alloc&) {
+            rm(mtx, i);
+            throw;
+        }
+    }
+    return mtx;
 }
 
 void output(const int * const * mtx) {
@@ -23,7 +32,12 @@ int main() {
         return 1;
     }
     int ** mtx = nullptr;
-    mtx = make(rows, cols);
+    try {
+        mtx = make(rows, cols);
+    } catch (const std::bad_alloc&) {
+        return 2;
+    }
+    
     output(mtx);
     rm(mtx, rows);
 
