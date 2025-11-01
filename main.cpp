5bc204ffa926d1f3cd50ce50e2c5246b8e073530
Jaroslaw-super1 <Jarafan3@gmail.com>
2025-11-01 19:39:53 +0500

    homework is ready

--- main.cpp
+++ main.cpp
@@ -1,90 +1,96 @@
 #include <iostream>
 
 
-void rm(const int * const * mtx, int r) {
-    for (size_t i = 0; i < r; i++) {
-        delete[] mtx[i];
-    }
+void rm(const int * const * mtx, size_t r)
+{
+  for (size_t i = 0; i < r; i++)
+  {
+    delete[] mtx[i];
+  }
 }
 
-int ** make(int r, const size_t * lns) {
-    int ** mtx = new int * [r];
-    for (size_t i = 0; i < r; i++) {
-        try {
-            mtx[i] = new int[lns[i] - 1];
-        } catch (const std::bad_alloc&) {
-            rm(mtx, i);
-            throw;
-        }
+int ** make(int r, const size_t * lns)
+{
+  int ** mtx = new int * [r];
+  for (size_t i = 0; i < r; i++)
+  {
+    try
+    {
+      mtx[i] = new int[lns[i] - 1];
+    } catch (const std::bad_alloc&)
+    {
+      rm(mtx, i);
+      throw;
     }
-    return mtx;
+  }
+  return mtx;
 }
 
-//void output(const int * const * mtx, int r, int c) {
-//    for (size_t i = 0; i < r; i++) {
-//        for (size_t j = 0; j < c; j++) {
-//            std::cout << mtx[i][j];
-//        }
-//        std::cout << "\n";
-//    }
-//}
-
-
-
-void input(int ** mtx, int r, int c) {
-
-    for (size_t i = 0; i < r; i++) {
-        for (size_t j = 0; j < c; j++) {
-            std::cin >> mtx[i][j];
-        }
+void output(const int * const * mtx, int r, const size_t * lns)
+{
+  int cnt = 0;
+  for (size_t i = 0; i < r; i++)
+  {
+    for (size_t j = 0; j < lns[i]; j++)
+    {
+      std::cout << mtx[i][j];
+      if (j + 1 != lns[i])
+      {
+        std::cout << " ";
+      }
     }
+    cnt += lns[i];
+    std::cout << "\n";
+  }
 }
 
+void input(int ** mtx, const int * t, size_t r, const size_t * lns)
+{
+  int cnt = 0;
+  for (size_t i = 0; i < r; i++)
+  {
+    for (size_t j = 0; j < lns[i]; j++)
+    {
+      mtx[i][j] = t[cnt + j];
+    }
+    cnt += lns[i];
+  }
+}
 
-/*
-int t[] = {5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8};
-size_t lns[] = {4, 2, 5,1};
-
-rows - элементов
-t - массив с элементами
-lns - массив с количеством элементов  в каждо строке
-
-*/
-int ** convert(const int * t, size_t n, const size_t * lns, size_t rows) {
-    int ** new_mtx = nullptr;
+int ** convert(const int * t, size_t n, const size_t * lns, size_t rows)
+{
+  int ** new_mtx = nullptr;
+  try
+  {
     new_mtx = make(rows, lns);
+    input(new_mtx, t, rows, lns);
     return new_mtx;
+  } catch (...)
+  {
+    throw;
+  }
 }
 
-int main() {
-    
-    int t1[] = {5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8};
-    size_t lns1[] = {4, 2, 5,1};
-    size_t rows = 4;
-    size_t n = 1;
+int main() 
+{
+  int t1[] = {5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8};
+  size_t lns1[] = {4, 2, 5, 1};
+  size_t rows = 4;
+  size_t n = 1;
 
-    int * t = t1;
-    size_t * lns = lns1;
+  int * t = t1;
+  size_t * lns = lns1;
     
-    int ** mtx = nullptr;
-    try {
-        mtx = convert(t, n, lns, rows);
-    } catch (const std::bad_alloc&) {
-        rm(mtx, rows);
-        return 2;
-    }
-
-    std::cout << mtx;
-
-    //if (std::cin.fail()) {
-    //    rm(mtx, rows);
-    //    return 1;
-    //}
-    //output(mtx, rows, cols);
-    
+  int ** mtx = nullptr;
+  try {
+    mtx = convert(t, n, lns, rows);
+  } catch (...) {
     rm(mtx, rows);
-
-    return 0;
+    return 1;
+  }
+  output(mtx, rows, lns);
+  rm(mtx, rows);
+  return 0;
 }
 
 
